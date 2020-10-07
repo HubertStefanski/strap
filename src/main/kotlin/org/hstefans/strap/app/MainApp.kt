f39d3c3ec057f58c3d95831ea3fdac8b1f175ef5
Hubert Stefanski <hstefans@redhat.com>
2020-10-07 21:24:58 +0100

    initial kotlin & tornadofx setup

---
+++ src/main/kotlin/org/hstefans/strap/app/MainApp.kt
@@ -0,0 +1,7 @@
+package org.hstefans.strap.app
+
+import org.hstefans.strap.view.MainView
+import tornadofx.*
+
+class MainApp : App(MainView::class) {
+}
---
+++ src/main/kotlin/org/hstefans/strap/view/MainView.kt
@@ -0,0 +1,11 @@
+package org.hstefans.strap.view
+
+
+import tornadofx.*
+
+
+class MainView : View() {
+    override val root = group {
+        label("hello world")
+    }
+}
